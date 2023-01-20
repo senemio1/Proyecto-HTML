dd8e6338fe32ba77180d42185c725e63671e40aa
Juan - PC Poroto <juan.martin2002@hotmail.com>
2023-01-20 20:54:14 -0300

    Javascript de popup, popup y algunas clases de css nuevas para el javascript

--- Scripts/misscripts.js
+++ Scripts/misscripts.js
@@ -1,11 +1,27 @@
-function mifuncion(division) 
-{
-    var x= division;
-    if(x.style.display==="none")
-    {
-        x.style.display="block";
-    } else
-    {
-        x.style.display="none";
+function mostrarOcultar(divisor, button) {
+    var x = divisor;
+    var y = button;
+    if (x.style.display == "block") {
+        x.style.display = "none";
+        y.className = "fa-solid fa-plus fa-fw fa-xl margen_derecho texto_grisosc";
+    } else {
+        x.style.display = "block";
+        y.className = "fa-solid fa-minus fa-fw fa-xl margen_derecho texto_grisosc";
     }
 }
+
+function abrirPopup(cuerpo) {
+    let popup = document.getElementById("popup");
+    let body=cuerpo;
+
+    popup.classList.add("abrir-popup");
+    body.classList.add("overlay");
+}
+function cerrarPopup(cuerpo) {
+    let popup = document.getElementById("popup");
+    let body=cuerpo;
+    
+    popup.classList.remove("abrir-popup");
+    body.classList.remove("overlay");
+}
+
